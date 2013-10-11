890c54f03141f1439da2a8a81b4da5409247fa9d
lyaunzbe <lyaunzon.b@gmail.com>
2013-10-11 17:48:11 -0400

    Work on the projects section + circle viz

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,132 @@
 var bg = ['http://upload.wikimedia.org/wikipedia/commons/8/81/Downtown_LA_from_GP_Obs.jpg',
           'http://i.imgur.com/tkkNO.jpg'];
 
-var bg_text = ['Design.', 'Create.', 'Build.'],
-    bgt_count = 0;
+var bgt_count = 0;
+
+d3.selection.prototype.moveToFront = function() {
+  return this.each(function(){
+    this.parentNode.appendChild(this);
+  });
+};
+
+$('project-exp').on('itemSelect', function(e, data){
+  console.log(data);
+});
 $(function(){
+  // Load a random high res bg
   $('.intro').css('background', 'url("'+bg[Math.floor(Math.random()*bg.length)]+'") no-repeat center center fixed');
   $('.intro').css('background-size', 'cover');
 
-  // setInterval(function(){
-  //   $('.intro .inner').fadeOut('1600', function(){
-  //     $('.intro .inner').text(bg_text[bgt_count]);
-  //     bgt_count = (bgt_count < bg_text.length - 1) ? bgt_count + 1 : 0;
-  //     $('.intro .inner').fadeIn('3000');
-  //   });
-  // },6000);
+  // Initialize dynamic text replacement
+  $('.inner').dynamo({
+    delay: Math.floor((Math.random()*5000)+3000)
+  });
+
+  // Initialize force graph
+  function forceGraph(data){
+    var w = 1292,
+    h = 700,
+    nodes = [{ id:3, size:100, name:'lute', url:''}, { id:6, size:70, name:'node-fliptop', url:''}, 
+             {id:4, size:80, name: 'derrida', url:''}, { id:5, size:60, name:'fadeAway', url:''}, 
+             { id:1, size:150, name:'mffm', url:''}, { id:2, size:130, name:'hiphopJS', url:''},],
+    node;
+
+    function charge(d) {
+      return -Math.pow(d.size, 2) / 4;
+    }
+
+    var force = d3.layout.force()
+      .nodes(nodes)
+      .size([w, h])
+      .links([])
+      .on("tick", tick)
+      .charge(charge);
+
+
+    var vis = d3.select(".project-wrap").append("svg")
+        .attr("width", w)
+        .attr("height", h)
+        .style("position", "absolute");
+
+    function tick(e) {
+
+      var k = .1 * e.alpha;
+      nodes.forEach(function(o, i) {
+        o.y += (300 - o.y) * k;
+        o.x += (100 - o.x) * k;
+      });
+
+      node.attr("transform",function(d){ return "translate("+d.x+","+d.y+")";});
+    };
+
+
+    // Update the nodes…
+    node = vis.selectAll("g")
+        .data(nodes)
+        .attr("r", function(d) { return d.size; });
+
+    // Enter any new nodes.
+    
+    node.enter().append("g").attr("class", "circle")
+        .on("mouseover",function(){
+          var self = this,
+              prev = d3.selectAll("g.active"),
+              sel = d3.select(self);
+
+          //Remove style for old circle
+          prev.select('circle').style("fill", function(d){ return 'rgba(51,51,51,'+d.size/200+')';})
+            .style("stroke", function(d) { return d3.rgb(61,61,61); }).style("stroke-width", 2);
+          prev.select('text').style("fill", "rgba(49, 49, 49, 0.75)");
+
+          //Style new active circle and move it to front
+          sel.attr('class', 'circle active')
+             .select('circle').style("fill", "rgba(31,31,51,.85)")
+             .style("stroke", function(d){return d3.rgb(231, 231, 231)}).style("stroke-width", 5);
+          sel.select('text').style("fill", "rgba(231, 231, 231, 0.75)");
+          sel.moveToFront();
+
+          // Active right info panel. Make sure to ignore extra mouseover triggers.
+          if( $('.project-exp li.active').length > 0){ 
+            var itemID = $('.project-exp li.active').attr('id') ? 
+                          parseInt($('.project-exp li.active').attr('id')) : null;
+
+            if( (itemID && itemID !== d3.select(self).datum().id) || !itemID ){
+              $('.project-exp li.active').removeClass('active').fadeOut(300, function(){
+                $('.project-exp #'+d3.select(self).datum().id)
+                  .addClass('active').hide().fadeIn(300);
+              });
+            }
+            
+          }
+        })
+        .on("mousedown", function(){
+          d3.event.stopPropagation();
+          nodes.forEach(function(o, i) {
+            o.x += (Math.random() - .5) * ((Math.random()*100)+40);
+            o.y += (Math.random() - .5) * ((Math.random()*100)+40);
+          });
+          force.resume();
+        })
+        .attr("transform",function(d){ return "translate("+d.x+","+d.y+")";})
+        .call(force.drag)
+        .append("a")
+        .attr("xlink:href", function(d){ return d.url})
+        .append("circle")
+        .attr("r", function(d) { return d.size; })
+        .style("fill", function(d){ return 'rgba(51,51,51,'+d.size/200+')';})
+        .style("stroke", function(d) { return d3.rgb(61,61,61); })
+        .style("stroke-width", 2);
+
+        node
+        .append("a")
+          .attr("xlink:href", function(d){ return d.url})
+          .append("text")
+          .text(function(d){return d.name})
+          .attr("font-size", function(d){return 22*d.size/100+"px";});
+
+    // Restart the force layout.
+    force.start();
+  }
+  forceGraph();
+
 });
