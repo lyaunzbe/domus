f4950270219c487b78cd09762e6c410f9dda63c3
lyaunzbe <lyaunzon.b@gmail.com>
2013-10-29 21:12:21 -0400

    Added btx

---
+++ public/btx/js/main.js
@@ -0,0 +1,39 @@
+$(function(){
+  $.support.cors = true;
+
+  function roundCurr(num) {
+    return Math.round(num * 1000) / 1000;
+  }
+
+  var base = 'http://0.0.0.0:3737/'
+  
+  $('.submit').click(function(){
+    
+    var data = {
+      amount: $('.amt').val()
+    };
+
+    console.log(data);
+    $.ajax({
+      type: "POST",
+      url: base + 'quote',
+      data: data,
+      dataType: 'json',
+      crossDomain: true
+    }).done(function(result){
+      $('.buy span').empty();
+      $('.sell span').empty();
+
+      $('.buy .q').append(' '+roundCurr(result.buy.valBTC)+' BTC / '+
+        roundCurr(result.buy.valUSD)+' USD');
+      $('.buy .x').append(' '+roundCurr(result.buy.rate)+' USD => 1 BTC');
+      $('.buy .d').append(result.buy.depth);
+
+      $('.sell .q').append(' '+roundCurr(result.sell.valBTC)+' BTC / '+
+        roundCurr(result.sell.valUSD)+' USD');
+      $('.sell .x').append(' '+roundCurr(result.sell.rate)+' USD => 1 BTC');
+      $('.sell .d').append(result.sell.depth);
+    })
+  })
+
+});
